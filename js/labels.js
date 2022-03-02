e5b0b99456a0c702a9e17f25e467206eba2b3ee3
KHAREETAENGINEERING <99054761+KHAREETAENGINEERING@users.noreply.github.com>
2022-03-02 04:25:42 +0300

    Update labels.js

--- js/labels.js
+++ js/labels.js
@@ -1,9 +1,9 @@
 var hideLabel = function(label) {
-    label.labelObject.style.opacity = 0;
+    label.labelObject.style.opacity = 1;
     label.labelObject.style.transition = 'opacity 0s';
 };
 var showLabel = function(label) {
-    label.labelObject.style.opacity = 1;
+    label.labelObject.style.opacity = 3;
     label.labelObject.style.transition = 'opacity 1s';
 };
 labelEngine = new labelgun.default(hideLabel, showLabel);
